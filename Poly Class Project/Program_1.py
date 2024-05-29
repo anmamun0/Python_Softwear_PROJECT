d322419c1f3cd263d9df6cca35943916b0f5185a
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-29 23:18:16 +0600

    Delete Poly Class Project directory

--- Poly Class Project/Program_1.py
+++
@@ -1,9 +0,0 @@
-class Calculator:
-    def __init__(self, a, b):
-        print(f'All Calculation in down = {a} @ {b} \n')
-        print(f' \nAddition: {a + b}')
-        print(f'Substraction: {a + b}')
-        print(f'Multiplication: {a + b}')
-        print(f'Division: {a + b}\n')
-
-cal = Calculator(15,5)
